a35e4c06b9f1ba21d4a97fc85a0975f01a1dd21a
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2025-09-13 13:03:04 +0530

    Update project structure with new navigation and pages; replace placeholder content with actual components for Home and About Us, and add React Router for navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Navigation from "./components/Navigation";
 import "./App.css";
 
 function App() {
-
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <div className="min-h-screen min-w-screen bg-gray-50 w-full">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
